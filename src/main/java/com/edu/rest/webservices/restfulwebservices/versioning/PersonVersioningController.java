83e2ce546285cf7e7a14c58391c7f70a3421fb69
metabrez <metabrez@gmail.com>
2018-01-12 19:34:50 -0600

    This is versioning using param

--- src/main/java/com/edu/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/edu/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -17,4 +17,20 @@ public class PersonVersioningController {
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Tabrez","Alam"));
 	}
+	
+	@GetMapping(value="/person/param",params="version=1")
+
+	public PersonV1 paramV1() {
+		
+		return new PersonV1("Tabrez Alam");
+		
+	}
+	
+	@GetMapping(value="/person/param",params="version=2")
+	public PersonV2 paramV2() {
+		
+		return new PersonV2(new Name("Tabrez","Alam"));
+		
+	}
+	
 }
