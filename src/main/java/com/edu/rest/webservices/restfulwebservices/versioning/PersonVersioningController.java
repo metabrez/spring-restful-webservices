4d16d1ba551b308e60783f3239c600a18d374ce8
metabrez <metabrez@gmail.com>
2018-01-12 20:21:18 -0600

    Different types of versioning

--- src/main/java/com/edu/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
+++ src/main/java/com/edu/rest/webservices/restfulwebservices/versioning/PersonVersioningController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PersonVersioningController {
 
-	
+	//URI versioning
 	@GetMapping("v1/person")
 	public PersonV1 personV1() {
 		
@@ -17,20 +17,39 @@ public class PersonVersioningController {
 	public PersonV2 personV2() {
 		return new PersonV2(new Name("Tabrez","Alam"));
 	}
-	
-	@GetMapping(value="/person/param",params="version=1")
-
+	//Parameter versioning
+	@GetMapping(value = "/person/param", params = "version=1")
 	public PersonV1 paramV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(value = "/person/param", params = "version=2")
+	public PersonV2 paramV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+//Media Type versioning or Content negotation or accept header versioning
+	@GetMapping(value="/person/header",headers="X-API-VERSION=1")
+
+	public PersonV1 headerV1() {
 		
 		return new PersonV1("Tabrez Alam");
 		
 	}
-	
-	@GetMapping(value="/person/param",params="version=2")
-	public PersonV2 paramV2() {
+	//Media Type versioning or Content negotation or accept header versioning
+	@GetMapping(value="/person/header",headers="X-API-VERSION=2")
+	public PersonV2 headerV2() {
 		
 		return new PersonV2(new Name("Tabrez","Alam"));
 		
 	}
-	
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v1+json")
+	public PersonV1 producesV1() {
+		return new PersonV1("Bob Charlie");
+	}
+
+	@GetMapping(value = "/person/produces", produces = "application/vnd.company.app-v2+json")
+	public PersonV2 producesV2() {
+		return new PersonV2(new Name("Bob", "Charlie"));
+	}
+
 }
